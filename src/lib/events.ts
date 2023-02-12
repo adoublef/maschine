1a5e5b1b191492b428eb82910b672c731d09bbee
aboublef <kristopherab@gmail.com>
2023-02-12 14:24:18 +0000

    added custom events helper

--- src/lib/events.ts
+++ src/lib/events.ts
@@ -1,1 +1,5 @@
 export { };
+
+export function customEvent<T = {}>(el: HTMLElement, type: string, detail?: T) {
+    return el.dispatchEvent(new CustomEvent(type, { detail, bubbles: true, composed: true, }));
+}
