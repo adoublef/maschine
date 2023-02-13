de234ba8e81c94f7c660d8d0a77779279de5a077
aboublef <kristopherab@gmail.com>
2023-02-13 11:48:12 +0000

    .

--- src/lib/events.ts
+++ src/lib/events.ts
@@ -1,5 +1,3 @@
-export { };
-
 export function customEvent<T = {}>(el: HTMLElement, type: string, detail?: T) {
     return el.dispatchEvent(new CustomEvent(type, { detail, bubbles: true, composed: true, }));
 }
