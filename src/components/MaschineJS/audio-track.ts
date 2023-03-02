f5c217e51ea0f4dfa914c4fe5939c9f86433e144
Kristopher Rahim <kristopherab@gmail.com>
2023-03-02 00:59:02 +0000

    audio plays via context

--- src/components/MaschineJS/audio-track.ts
+++ src/components/MaschineJS/audio-track.ts
@@ -9,12 +9,42 @@ export class AudioTrack extends LitElement {
     @property({ attribute: false })
     public audioContext?: AudioContext;
 
+    @property()
+    public src?: string;
+
     protected render(): unknown {
         if (!this.audioContext)
             throw new Error("no context");
 
         return html`
-            <div>My Audio Track</div>
+            <audio>
+                <source src=${this.src}>
+            </audio>
+            <button @click=${this.play}>My Audio Track</button>
         `;
+    }
+
+    public async play() {
+        if (!this.audioContext)
+            throw new Error("no context");
+
+        if (!this.src)
+            throw new Error("no src path");
+
+        try {
+            const response = await fetch(this.src);
+            const buffer = await response.arrayBuffer();
+
+            const audioBuffer = await this.audioContext.decodeAudioData(buffer);
+
+            const source = new AudioBufferSourceNode(this.audioContext, {
+                buffer: audioBuffer
+            });
+
+            source.connect(this.audioContext.destination);
+            source.start();
+        } catch (error) {
+            console.error((error as Error).message);
+        }
     }
 }
