7590d66fdcc5bb67ed6139a8e2a9e933d1e9b901
Kristopher Rahim <kristopherab@gmail.com>
2023-03-02 01:49:10 +0000

    adding caching

--- src/components/MaschineJS/audio-track.ts
+++ src/components/MaschineJS/audio-track.ts
@@ -3,6 +3,8 @@ import { html, LitElement } from "lit";
 import { customElement, property } from "lit/decorators.js";
 import { audioContext } from "./audioContext";
 
+const cacheName = "fetch-audio";
+
 @customElement("audio-track")
 export class AudioTrack extends LitElement {
     @consume({ context: audioContext })
@@ -17,34 +19,61 @@ export class AudioTrack extends LitElement {
             throw new Error("no context");
 
         return html`
-            <audio>
+            <audio preload="auto">
                 <source src=${this.src}>
             </audio>
-            <button @click=${this.play}>My Audio Track</button>
+            <button type="button" @click=${this.play}>
+                <slot>Audio Track</slot>
+            </button>
         `;
     }
 
     public async play() {
-        if (!this.audioContext)
-            throw new Error("no context");
+        const { src, audioContext } = this;
 
-        if (!this.src)
+        if (!src)
             throw new Error("no src path");
 
+        if (!audioContext)
+            throw new Error("no context");
+
         try {
-            const response = await fetch(this.src);
-            const buffer = await response.arrayBuffer();
+            const source = await createBufferSource(src, audioContext);
 
-            const audioBuffer = await this.audioContext.decodeAudioData(buffer);
-
-            const source = new AudioBufferSourceNode(this.audioContext, {
-                buffer: audioBuffer
-            });
-
-            source.connect(this.audioContext.destination);
+            source.connect(audioContext.destination);
             source.start();
         } catch (error) {
             console.error((error as Error).message);
         }
     }
 }
+
+async function fetchCache(input: string, init?: RequestInit) {
+    // NOTE -- open cache
+    const cache = await caches.open(cacheName);
+
+    // NOTE -- if in cache return response
+    let response = await cache.match(input);
+    if (response) return response.clone();
+
+
+    response = await fetch(input, init);
+    await cache.put(input, response.clone());
+
+    return response;
+}
+
+const createBufferSource = async (src: string, audioContext: AudioContext): Promise<AudioBufferSourceNode> => {
+    try {
+        const response = await fetchCache(src);
+        const buffer = await response.arrayBuffer();
+
+        const audioBuffer = await audioContext.decodeAudioData(buffer);
+
+        return new AudioBufferSourceNode(audioContext, { buffer: audioBuffer });
+
+    } catch (error) {
+        // TODO -- custom error handling
+        throw error;
+    }
+};
